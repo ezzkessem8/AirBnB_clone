2d15a9979e21974d9d5e34703229aab8af63918a
ezzkessem8 <92451799+ezzkessem8@users.noreply.github.com>
2024-03-11 02:55:45 +0300

    Create state.py

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""State Module"""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """State class"""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes State instance"""
+        super().__init__(*args, **kwargs)
+        
